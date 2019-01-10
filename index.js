9666d0628c6cf517bce20c9b993b4c3ac72aff72
Nadeem Khedr <nadeemkhedr@gmail.com>
2019-01-10 09:07:59 +0800

    remove apidoc

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ const app = express()
 const PORT = process.env.PORT || '8000'
 
 
-/**
- * @api {get} /hello/{name} Prints "Hello {name}"
- * @apiName HelloWorld
- * @apiParam (Url) {String} name the name to print
- * @apiSuccess (200) {String} message the hello {name} message
- */
 app.get('/hello/:name', (req, res) =>
   res.send({
     message: `Hello ${req.params.name}`
