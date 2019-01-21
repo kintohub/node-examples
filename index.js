6e179c188c453eeb0f5da17439dddf61f962e208
Nadeem Khedr <nadeemkhedr@gmail.com>
2019-01-21 10:59:40 +0800

    ✨ feat(general): add a console log for the endpoint

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || '8000'
 
-
 /**
  * @api {get} /hello/{name} Prints "Hello {name}"
  * @apiName HelloWorld
  * @apiParam (Url) {String} name the name to print
  * @apiSuccess (200) {String} message the hello {name} message
  */
-app.get('/hello/:name', (req, res) =>
-  res.send({
+app.get('/hello/:name', (req, res) => {
+  console.log('Log request: ', req.params.name)
+  return res.send({
     message: `Hello ${req.params.name}`
   })
-)
+})
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
