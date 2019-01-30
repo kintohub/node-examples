2ae4e6828e40fc90102da2eb89c10035b7d53227
Nadeem Khedr <nadeemkhedr@gmail.com>
2019-01-30 08:56:12 +0800

    ✨ feat(general): add sample env variable to response

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const PORT = process.env.PORT || '8000'
 
 app.get('/hello/:name', (req, res) =>
   res.send({
-    message: `Hello ${req.params.name}`
+    message: `Hello ${req.params.name}`,
+    ENV_SAMPLE: process.env.ENV_SAMPLE
   })
 )
 
